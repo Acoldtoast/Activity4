cad93b24a3513f24af080bb7ce00f988182b5b4a
Ethan <ethansapuay222@gmail.com>
2025-12-01 00:28:48 +0800

    feat/modify-lastname-dr-reyes-gonzales

--- veterinary.sql
+++ veterinary.sql
@@ -139,3 +139,7 @@ ALTER TABLE invoices RENAME COLUMN paymentdate TO paymenttime;
 
 DELETE FROM appointments 
 WHERE animalid = (SELECT animalid FROM animals WHERE name = 'Simba');
+
+UPDATE doctors 
+SET dlastname = 'Reyes-Gonzales' 
+WHERE dfirstname = 'Dr.Sofia';
