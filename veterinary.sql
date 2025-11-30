304ca5076d3a51d0ab4ad386a6e940ee1dd959aa
Acoldtoast <temporaryemailforspams@gmail.com>
2025-12-01 00:28:30 +0800

    feat/remove-appointment-simba

--- veterinary.sql
+++ veterinary.sql
@@ -136,3 +136,6 @@ ALTER TABLE owners
 ADD COLUMN registereddate DATE;
 
 ALTER TABLE invoices RENAME COLUMN paymentdate TO paymenttime;
+
+DELETE FROM appointments 
+WHERE animalid = (SELECT animalid FROM animals WHERE name = 'Simba');
